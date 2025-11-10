c794d59bd071ccaeb1ce1de401d108040ec57559
Om Patil <omppatil2002@gmail.com>
2025-11-10 19:29:12 +0530

    ne c added

---
+++ data_types.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+    int Age = 23;
+    float Height = 5.9;
+    char Grade = 'A';
+
+    printf("Age: %d\n", Age);
+    printf("Height: %.2f\n", Height);
+    printf("Grade: %c", Grade);
+
+    return 0;
+}
